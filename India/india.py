70ba6bb66d8709394e8a3563f06cb66032fab941
Ananya Chatterjee <74227338+Ananya-0306@users.noreply.github.com>
2022-01-04 17:19:50 +0530

    Complete Code

---
+++ India/india.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+# @Author: Ananya Chatterjee
+# @Date: 04 January 2022
+# @Env: python 3.6 
+# @Github: https://github.com/ananya-0306 
+
+a = 10
+b = 0
+c = 10
+
+s = ("TFy!QJu ROo TNn(ROo)SLq SLq ULo+UHs"
+     " UJq TNn*RPn/QPbEWS_JSWQAIJO^NBELPe"
+     "HBFHT}TnALVlBLOFAkHFOuFETpHCStHAUFA"
+     "gcEAelclcn^r^r\\tZvYxXyT|S~Pn SPm S"
+     "On TNn ULo0ULo#ULo-WHq!WFs XDt!") 
+
+a = ord(s[b])
+  
+while a != 0:  
+    if b < 170:  
+        a = ord(s[b])  
+        b += 1
+          
+        while a > 64:  
+            a -= 1
+            c += 1
+              
+            if c == 90:  
+                c = c // 9
+                print(end = chr(c))  
+            else:  
+                print(chr(33 ^ (b & 0X01)), end = '')  
+    else:  
+        break
+  
